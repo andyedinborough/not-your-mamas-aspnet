4853d0ab3e305127b2efcfe8d127faf7bba8c321
Andy Edinborough <andy@edinborough.org>
2017-04-24 20:02:36 -0500

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#clean out the last publish
+rm -rf bin/
+
+#build
+dotnet publish -c Release
+
+#stop the service
+ssh -i ../andy.pem ubuntu@edinborough.org 'sudo service dotnetcore stop'
+
+#deploy
+rsync -uaz --delete --progress -e 'ssh -i ~/repos/andy.pem' bin/Release/netcoreapp1.1/publish/ ubuntu@edinborough.org:/home/ubuntu/dotnetcore
+
+#start web service
+ssh -i ../andy.pem ubuntu@edinborough.org 'sudo service dotnetcore start'
